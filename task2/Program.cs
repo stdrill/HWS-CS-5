f3b1275d883d2b7fd4ad9f1691fed964e9a94b1b
stdrill <tanya23.96@mail.ru>
2022-09-06 23:43:31 +0300

    task3 added

---
+++ task2/Program.cs
@@ -0,0 +1,16 @@
+﻿// Написать программу масштабирования фигуры 
+
+Console.WriteLine("Задайте координаты фигуры ");
+string coord = Console.ReadLine();
+string[] figure = coord.Split(" ");
+string result = "";
+Console.WriteLine("Введите коэффициент масштабирования");
+int k = Convert.ToInt32(Console.ReadLine());
+for(int i = 0; i < figure.Length; i++)
+{
+    string[] num = figure[i].Split(",");
+    for(int j = 0; j < figure.Length; j++)
+    {
+        num[j] = Convert.ToInt32(num[j].Replace("(","").Replace(")",""));
+    }
+}
---
+++ task3/Program.cs
@@ -0,0 +1,19 @@
+﻿// Написать программу копирования массива
+
+void FillArray(int[] arr)
+{
+    for(int i = 0; i < arr.Length; i++) arr[i] = new Random().Next(0,100);
+}
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++) Console.Write(arr[i]+" ");
+}
+
+int[] arr = new int[10];
+FillArray(arr);
+PrintArray(arr);
+Console.WriteLine();
+int[] copy = new int[arr.Length];
+arr.CopyTo(copy, 0);
+Console.WriteLine("Копия массива: ");
+Console.WriteLine(String.Join(" ", copy));
