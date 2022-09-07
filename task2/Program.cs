0f6720eadbc54569c2cfa16153c7a76622092982
stdrill <tanya23.96@mail.ru>
2022-09-07 22:33:21 +0300

    task2 added

--- task2/Program.cs
+++ task2/Program.cs
@@ -1,16 +1,22 @@
 ﻿// Написать программу масштабирования фигуры 
 
-Console.WriteLine("Задайте координаты фигуры ");
+Console.WriteLine("Задайте координаты фигуры в формате: (0,0) (1,0)...");
 string coord = Console.ReadLine();
 string[] figure = coord.Split(" ");
 string result = "";
 Console.WriteLine("Введите коэффициент масштабирования");
-int k = Convert.ToInt32(Console.ReadLine());
+int k = Convert.ToInt32(Console.ReadLine()); 
 for(int i = 0; i < figure.Length; i++)
 {
+    // Console.WriteLine(figure[i]);
     string[] num = figure[i].Split(",");
-    for(int j = 0; j < figure.Length; j++)
+    result = result + "(";
+    for(int j = 0; j < num.Length; j++)
     {
-        num[j] = Convert.ToInt32(num[j].Replace("(","").Replace(")",""));
+        int newCoordinate = Convert.ToInt32(num[j].Replace("(","").Replace(")","")) * k;
+        if(j == 0) result = result + $"{newCoordinate},";
+        else result = result + $"{newCoordinate}";
     }
+    result = result + ") ";
 }
+Console.WriteLine(result);
